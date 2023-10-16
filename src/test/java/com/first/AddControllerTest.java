d803bd0bc184687fdc502875d60b817fe1bbb6bf
mohd-arslan <92792383+mohd-arslan@users.noreply.github.com>
2023-10-16 10:50:08 +0530

    Update AddControllerTest.java

--- src/test/java/com/first/AddControllerTest.java
+++ src/test/java/com/first/AddControllerTest.java
@@ -1,44 +1,89 @@
 package com.first;
 
+ 
+
 import static org.junit.Assert.assertEquals;
+
+ 
+
 import org.junit.Test;
 
-class AddControllerTest {
-	
+ 
 
-	@Test
-	void testAdd() {
-		AddController addctrl = new AddController();
-		double actual = addctrl.add(10,10);
-		double expected = 20;
-		assertEquals(expected , actual);
-	   	}
+public class AddControllerTest {
 
-	@Test
-	void testSub() {
-		AddController addctrl = new AddController();
-		double actual = addctrl.sub(30,10);
-		double expected = 20;
-		assertEquals(expected , actual);
-	   	}
-	
+   
 
-	@Test
-	void testMultiply() {
-		AddController addctrl = new AddController();
-		double actual = addctrl.mul(10,10);
-		double expected = 100;
-		assertEquals(expected , actual);
-	   	
-	}
+ 
 
-	@Test
-	void testDivide() {
-		AddController addctrl = new AddController();
-		double actual = addctrl.div(10,10);
-		double expected = 1;
-		assertEquals(expected , actual);
-	   	
-	}
+    @Test
+
+    public void testAdd() {
+
+        AddController addctrl = new AddController();
+
+        double actual = addctrl.add(10,10);
+
+        double expected = 20;
+
+        assertEquals(expected,actual,0);
+
+        }
+
+ 
+
+    @Test
+
+    public void testSub() {
+
+        AddController addctrl = new AddController();
+
+        double actual = addctrl.sub(30,10);
+
+        double expected = 20;
+
+        assertEquals(expected , actual,0);
+
+        }
+
+   
+
+ 
+
+    @Test
+
+    public void testMultiply() {
+
+        AddController addctrl = new AddController();
+
+        double actual = addctrl.mul(10,10);
+
+        double expected = 100;
+
+        assertEquals(expected , actual,0);
+
+       
+
+    }
+
+ 
+
+    @Test
+
+    public void testDivide() {
+
+        AddController addctrl = new AddController();
+
+        double actual = addctrl.div(10,10);
+
+        double expected = 1;
+
+        assertEquals(expected , actual,0);
+
+       
+
+    }
+
+ 
 
 }
